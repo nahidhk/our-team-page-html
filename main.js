62e89f8bf94844971b3cc6deb1d65630cb3ac8ae
nahidappltd@gmail.com <nahidappltd@gmail.com>
2024-01-09 10:41:08 +0600

    new update

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 console.log('Hello World!');
-function hist(){
- window.location.href="/history"
+
+function hist() {
+  window.location.href = "/history"
+}
+
+function mein() {
+  document.getElementById("otk").style = `
+  display: block;
+  height: auto;
+  width: 100%;
+  `
+}
+
+function cal() {
+  document.getElementById("otk").style = `
+  display: none;
+  height: auto;
+  width: 100%;
+  `
 }
---
+++ nav.js
@@ -0,0 +1,170 @@
+var nav = `
+ <section id="otk" class="bkt">
+    
+   <div onclick="cal()" class="user-nav si">
+    <i  class="bi bi-x-lg"></i>
+   </div>
+    <br><br><br><br>
+    <center>
+      
+   <div class="user">
+     <br>
+     <img class="uimg" src="/user/nahid.png"/>
+     <span class="title">Nahidul Islam </span>
+     <blockquote>
+ <p>Website Developer , Logo and Graphics , Audio And Video editing and Electric service department.</p>    
+      </blockquote>
+ <button onclick="window.location.href='tel:+8801877357091'" class="hus">Contact <i class="bi bi-telephone"></i> +8801877357091 </button>  
+ 
+ <br><br>
+<div class="icon rx">
+  
+<!--- Facebook -->
+<a href="https://www.facebook.com/nahid.hk.bd">
+  
+<i class="bi bi-facebook"></i>
+</a>
+
+<!-- Instagram -->
+<a href="https://www.instagram.com/nahidhk0">
+
+<i class="bi bi-instagram"></i>
+</a>
+<!-- Twitter -->
+<a href="https://www.x.com/NAHIDHK0">
+
+<i class="bi bi-twitter-x"></i>
+</a>
+<!---- data list --->
+<a href="https://github.com/nahidhk">
+
+<i class="bi bi-github"></i>
+</a>
+<!---- linkedin--->
+<a href="https://www.linkedin.com/in/nahidhk">
+
+<i class="bi bi-linkedin"></i>
+</a>
+<!---email--->
+<a href="mailto: info@nahidhk.info">
+
+<i class="bi bi-envelope"></i>
+</a>
+
+
+
+</div>
+  
+   </div>
+   <br><br>  
+<!---- mamun---->
+
+
+  <div class="user">
+     <br>
+     <img class="uimg" src="/user/mamun.jpg"/>
+     <span class="title">Abir Islam</span>
+ <p>onec Photo Editor Department.</p>    
+ 
+ <button onclick="window.location.href='tel:+8801748158136'" class="hus">Contact <i class="bi bi-telephone"></i> +8801748158136 </button>  
+ 
+ <br><br>
+<div class="icon rx">
+  
+<!--- Facebook -->
+<a href="https://www.facebook.com/profile.php?id=100087193120668&mibextid=zLoPMf">
+  
+<i class="bi bi-facebook"></i>
+</a>
+
+<!---email--->
+<a href="mailto:mamun@nahidhk.info">
+
+<i class="bi bi-envelope"></i>
+</a>
+
+</div>
+   </div>
+     <br><br><br>
+      <a href="ujobs" class="link"> Invest Your Skills in Nahid HK™ For job Your skills Department!</a>
+</center>
+
+   <br><br><br>
+  </section>
+ <section class="nav7">
+ <a href="https://www.nahidhk.info/">
+   <img style="height: 80px;" src="/img/logo.png"/>
+   </a>
+   <div onclick="mein()" class="user-nav">
+    <i class="bi bi-people"></i>
+   </div>
+   <br>
+ </section> 
+
+`
+
+var foter = `
+
+  
+<!-- the foter side-->
+<section class="foter">
+   <div class="ssi">
+    <img style="height: 80px;" src="/img/logo.png"/>
+   
+ </div>
+  <center>
+    
+  <div>
+    <i class="bi bi-geo-alt"></i>
+   <br>Ataikula Bazar, Pabna, Rajshahi 
+  </div>
+<br>
+
+<div class="icon">
+  
+<!--- Facebook -->
+<a href="https://www.facebook.com/nahid.hk.bd">
+  
+<i class="bi bi-facebook"></i>
+</a>
+
+<!-- Instagram -->
+<a href="https://www.instagram.com/nahidhk0">
+
+<i class="bi bi-instagram"></i>
+</a>
+<!-- Twitter -->
+<a href="https://www.x.com/NAHIDHK0">
+
+<i class="bi bi-twitter-x"></i>
+</a>
+<!---- data list --->
+<a href="https://github.com/nahidhk">
+
+<i class="bi bi-github"></i>
+</a>
+<!---- linkedin--->
+<a href="https://www.linkedin.com/in/nahidhk">
+
+<i class="bi bi-linkedin"></i>
+</a>
+<!---email--->
+<a href="mailto: info@nahidhk.info">
+
+<i class="bi bi-envelope"></i>
+</a>
+
+
+
+</div>
+  
+  
+ Copyright &copy; 2021 - 2024 Nahid HK   
+
+    </center>
+</section>
+
+`
+
+document.getElementById("foter").innerHTML = foter;
+document.getElementById("nav").innerHTML = nav;
